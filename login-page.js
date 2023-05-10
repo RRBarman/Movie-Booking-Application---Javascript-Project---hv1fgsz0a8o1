7da9f3ea37650e16c1f716300ff4eeca874fe6e9
RAUNAK ROY BARMAN <48653401+RRBarman@users.noreply.github.com>
2023-05-10 18:40:41 +0530

    normal login update

--- login-page.js
+++ login-page.js
@@ -1,21 +1,15 @@
 const loginForm = document.getElementById("login-form");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const button_change = document.getElementById("form");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    let user = {
-        username: username,
-        password: password,
-    }
-    localstorage.setItem('user', JSON.stringify(user));
 
 
-    if (username === user.username && password === user.password) {
+    if (username === "user" && password === "web-dev") {
         alert("You have successfully logged in.");
-        localstorage.clear();
+        //localstorage.clear();
         window.location.replace("/index.html");
     } else {
         loginErrorMsg.style.opacity = 1;
