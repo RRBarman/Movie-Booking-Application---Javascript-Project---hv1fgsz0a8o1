b7683aaeef0161eaedb69e99fd912333f3cd0be5
RAUNAK ROY BARMAN <48653401+RRBarman@users.noreply.github.com>
2023-05-11 12:41:23 +0530

    same functionality in login user is updated.

--- login-page.js
+++ login-page.js
@@ -1,4 +1,4 @@
-const loginForm = document.getElementById("login-form");
+/*const loginForm = document.getElementById("login-form");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
@@ -10,6 +10,23 @@ loginButton.addEventListener("click", (e) => {
     if (username && password) {
         alert("You have successfully logged in.");
         //localstorage.clear();
+        window.location.replace("/index.html");
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})*/
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username && password) {
+        alert("You have successfully logged in.");
         window.location.replace("/index.html");
     } else {
         loginErrorMsg.style.opacity = 1;
